c1b751cfaa671974aa562695fe948c4445eea800
nurimantjiptarto <nkt5@njit.edu>
2021-03-14 06:52:57 -0400

    adding

---
+++ productList.js
@@ -0,0 +1,8 @@
+var mockDatabase = [
+    { _id: '123', name: 'Heaven Whipped Cream', rating: '5 stars' },
+    { _id: '583', name: 'Utensils Set', rating: '5 stars' },
+    { _id: '954', name: 'Candle: Rose Scent', rating: '4 stars' },
+    { _id: '384', name: 'Maui Hair Shampoo', rating: '3 stars' },
+    { _id: '183', name: 'Maui Hair Conditioner', rating: '3 stars' },
+    { _id: '007', name: 'Linen Set', rating: '4 stars' },
+];
